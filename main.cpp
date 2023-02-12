c53d7d71a5a806a328364307cee35227a250613c
wuchenhi <1634980701@qq.com>
2023-02-12 21:33:00 +0800

    view

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <vector>
+#include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -7,9 +10,60 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
-Model *model = NULL;
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
+
 const int width  = 800;
 const int height = 800;
+const int depth  = 250;
+
+Model *model = NULL;
+int *zbuffer = NULL;
+Vec3f light_dir = Vec3f(1,-1,1).normalize();
+Vec3f eye(1, 1, 3);
+Vec3f center(0, 0, 0);
+
+// 4维矩阵转为点
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
+}
+
+//点转为4维矩阵
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+// 视口矩阵  双单元立方体 [-1，1]*[-1，1]*[-1，1] 映射到屏幕立方体 [x，x+w]*[y，y+h]*[0，d] 上
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
+
+// eye 眼睛（摄像机） 返回view
+Matrix lookat(Vec3f eye, Vec3f center, Vec3f up) {
+    Vec3f z = (eye-center).normalize();//z 为眼睛指向中心
+    Vec3f x = (up^z).normalize();
+    Vec3f y = (z^x).normalize();
+    Matrix res = Matrix::identity(4);
+    for (int i=0; i<3; i++) {
+        res[0][i] = x[i];
+        res[1][i] = y[i];
+        res[2][i] = z[i];
+        res[i][3] = -center[i];
+    }
+    return res;
+}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
     // 确保高度小于宽度  不然有孔
@@ -45,7 +99,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 //扫线法  1分为2
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+void triangle1(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
     if (t0.y==t1.y && t0.y==t2.y) return;
     // 按y升序
     if(t0.y > t1.y) std::swap(t0, t1);
@@ -66,37 +120,134 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
     }
 }
 
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2,TGAImage &image, int *zbuffer) {
+    if (t0.y==t1.y && t0.y==t2.y) return; 
+    if (t0.y>t1.y) { std::swap(t0, t1); std::swap(ity0, ity1); }
+    if (t0.y>t2.y) { std::swap(t0, t2); std::swap(ity0, ity2); }
+    if (t1.y>t2.y) { std::swap(t1, t2); std::swap(ity1, ity2); }
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height;
+        Vec3i A   =               t0  + Vec3f(t2-t0  )*alpha;
+        Vec3i B   = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
+                float ityA =               ity0 +   (ity2-ity0)*alpha;
+        float ityB = second_half ? ity1 +   (ity2-ity1)*beta : ity0 +   (ity1-ity0)*beta;
+       if (A.x>B.x) { std::swap(A, B); std::swap(ityA, ityB); }
+        for (int j=A.x; j<=B.x; j++) {
+            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+            Vec3i   P = Vec3f(A) + Vec3f(B-A)*phi;
+            float ityP =    ityA  + (ityB-ityA)*phi;
+            int idx = P.x+P.y*width;
+            if (P.x>=width||P.y>=height||P.x<0||P.y<0) continue;
+            if (zbuffer[idx]<P.z) {
+                zbuffer[idx] = P.z;
+                image.set(P.x, P.y, TGAColor(255, 255, 255)*ityP);
+            }
+        }
+    }
+}
+
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
+}
+
 int main(int argc, char** argv) {
-    
     if(argc == 2) {
         model = new Model(argv[1]);
     }else {
         model = new Model("obj/african_head/african_head.obj");
     }
 
-    TGAImage image(width, height, TGAImage::RGB);
+    zbuffer = new int[width*height];
+    for (int i=0; i<width*height; i++) {
+        zbuffer[i] = std::numeric_limits<int>::min();
+    }
+    
+    { // draw the model
+        Matrix ModelView  = lookat(eye, center, Vec3f(0,1,0));
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+        Projection[3][2] = -1.f/(eye-center).norm();
 
-    Vec3f light_dir(0, 0, -1);
+        std::cerr << ModelView << std::endl;
+        std::cerr << Projection << std::endl;
+        std::cerr << ViewPort << std::endl;
+        Matrix z = (ViewPort*Projection*ModelView);
+        std::cerr << z << std::endl;
 
-    for(int i=0; i<model->nfaces(); ++i) {
-        std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3]; 
-        Vec3f world_coords[3];
-        for(int j=0; j<3; ++j) {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);//屏幕坐标
-            world_coords[j]  = v; //世界坐标
-        } 
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);//三角形的法线可以简单地计算为其两条边的叉积
-        n.normalize();
-        float intensity = n*light_dir;//照明强度等于光矢量的标量乘积和给定三角形的法线
-        if (intensity>0) {
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i=0; i<model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            float intensity[3];
+            for (int j=0; j<3; j++) {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] =  Vec3f(ViewPort*Projection*ModelView*Matrix(v));//Viewport * Projection * View * Model * v.
+                world_coords[j]  = v;
+                intensity[j] = model->norm(i, j)*light_dir;
+            }
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], intensity[0], intensity[1], intensity[2], image, zbuffer);
+        }
+
+        image.flip_vertically();
+        image.write_tga_file("output.tga");
+    }
+
+    { // dump z-buffer
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i=0; i<width; i++) {
+            for (int j=0; j<height; j++) {
+                zbimage.set(i, j, TGAColor(zbuffer[i+j*width]));
+            }
+        }
+        zbimage.flip_vertically(); 
+        zbimage.write_tga_file("zbuffer.tga");
+    }
+    delete model;
+    delete [] zbuffer;
+    return 0;
+}
+
+/* zbuffer use
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);//寻找一个同时与 （ABx，ACx，PAx） 和 （ABy，ACy，PAy） 正交的向量 （u，v，1）  叉积满足
+    if (std::abs(u[2])>1e-2) // u[2]是整数。如果它是零，那么三角形ABC是退化的
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); //（1 ? u ? v，u，v）
+    return Vec3f(-1,1,1); // 在这种情况下产生负坐标，它将被光栅化器丢弃
+}
+
+void triangle2(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
         }
     }
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    delete model;
-    return 0;
-   
+    Vec3f P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];//对于我们想要绘制的每个像素，只需将其重心坐标乘以我们栅格化的三角形顶点的 z 值
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
 }
+*/
